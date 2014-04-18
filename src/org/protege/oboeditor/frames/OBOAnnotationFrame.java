242c862fa6cd874e6215b4fe16a26cc83bf6077d
hdietze <hdietze@Eaglet-2.local>
2014-04-18 16:46:00 -0700

    more annotations
    add: id, alternate id, subset, db xref

--- src/org/protege/oboeditor/frames/OBOAnnotationFrame.java
+++ src/org/protege/oboeditor/frames/OBOAnnotationFrame.java
@@ -19,6 +19,11 @@ public class OBOAnnotationFrame extends AbstractOWLFrame<OWLAnnotationSubject> {
         
         final OWLDataFactory factory = man.getModelManager().getOWLDataFactory();
         
+        addSection(new OBOAnnotationFrameSection(man, this, "Identifier",
+                factory.getOWLAnnotationProperty(OBOVocabulary.OBO_ID.getIRI()), 1, false));
+        addSection(new OBOAnnotationFrameSection(man, this, "Alternate Identifier",
+                factory.getOWLAnnotationProperty(OBOVocabulary.OBO_ALTERNATE_ID.getIRI()), -1, false));
+        
 		addSection(new OBOAnnotationFrameSection(man, this, "Definition",
                 factory.getOWLAnnotationProperty(OBOVocabulary.DEFINITION.getIRI()), 1, true));
         addSection(new OBOAnnotationFrameSection(man, this, "Exact synonym",
@@ -31,6 +36,10 @@ public class OBOAnnotationFrame extends AbstractOWLFrame<OWLAnnotationSubject> {
                 factory.getOWLAnnotationProperty(OBOVocabulary.HAS_NARROW_SYNONYM.getIRI()), -1, true));
         addSection(new OBOAnnotationFrameSection(man, this, "Comment",
                 factory.getRDFSComment(), 1, false));
+        addSection(new OBOAnnotationFrameSection(man, this, "Subset",
+        		factory.getOWLAnnotationProperty(OBOVocabulary.OBO_SUBSET.getIRI()), -1, true));
+        addSection(new OBOAnnotationFrameSection(man, this, "Database Cross References",
+        		factory.getOWLAnnotationProperty(OBOVocabulary.OBO_DBXREF.getIRI()), -1, false));
         refill();
     }
 }
